33c46d60ae190b123bcb934656be699ba3661e1c
traceannwhite <traceannwhite@gmail.com>
2021-08-23 10:25:14 -0500

    tripDetails model and schema added

--- models/tripDetails.js
+++ models/tripDetails.js
@@ -1,1 +1,16 @@
-// comment
+const { Schema, model } = require("../db/connection");
+
+const TripDetailsSchema = new Schema({
+  img: String,
+  packageName: { type: String, required: true },
+  description: { type: String, required: true },
+  vehicle: String,
+  hotel: String,
+  location: [{ type: Number }],
+  contactInfo: [{ type: String }],
+  payment: [],
+});
+
+const TripDetails = model("TripDetails", TripDetailsSchema);
+
+module.exports = TripDetails;
